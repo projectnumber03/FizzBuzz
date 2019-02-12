c9a98982a7646224e965fe4ff4614557479e8ee5
DShilov <abarakus@bk.ru>
2019-02-12 19:38:19 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++){
-            fizzBuzz(i);
-        }
+        for (int i = 1; i <= 100; i++) fizzBuzz(i);
     }
 
-    public static void fizzBuzz(int n){
+    public static void fizzBuzz(int n) {
         String answer = "";
-        if(n % 3 == 0) answer += FizzBuzz.Fizz;
-        if(n % 5 == 0) answer += FizzBuzz.Buzz;
+        if (n % 3 == 0) answer += FizzBuzz.Fizz;
+        if (n % 5 == 0) answer += FizzBuzz.Buzz;
         System.out.println(answer.equals("") ? n : answer);
     }
 }
